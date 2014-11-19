17d234a010246e0d4f1106517e6fcffbb0199ffc
Gian Mauricio Fritsche <gian.fritsche@gmail.com>
2014-11-19 09:59:05 -0200

    update run.sh

--- run.sh
+++ run.sh
@@ -12,38 +12,41 @@ OO_MyBatis"
 functions="ChoiceFunction
 MultiArmedBandit"
 
-alpha=1.0
-beta=0.021
+# alpha=1.0
+# beta=0.021
 
-w=12000
-c=7.0
+# w=12000
+# c=7.0
 
-objectivesArray="2
-4"
+# objectivesArray="2
+# 4"
 
 evaluations=60000
 population=300
 crossover=0.95
 mutation=0.02
 
-executions=10
+executions=30
 path="experiment/"
 
 rm -f run.txt
 
-for objectives in $objectivesArray
-do
+# for objectives in $objectivesArray
+# do
     #echo "java -cp dist/MECBA-Hyp.jar jmetal.experiments.Combined_NSGAII_"$objectives"obj" >> run.txt
     for function in $functions
     do
         for problem in $problems
         do
-            echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation $alpha $beta TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function $w $c $objectives false $executions $path" >> run.txt
+        	# 2 objectives
+            echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.1027 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 3.1043 $objectives false $executions $path" >> run.txt
+            # 4 objectives
+            echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.0223 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 2.2274 $objectives false $executions $path" >> run.txt
         done
     done
-done
+# done
 
-cat run.txt | xargs -I CMD -P 2 bash -c CMD >> output.log &
+cat run.txt | xargs -I CMD -P 8 bash -c CMD >> output.log &
 wait
 
 rm -f run.txt
