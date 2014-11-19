e5a47ee7eeda0e5ca7b02ce51df34e143fa06221
Gian Mauricio Fritsche <gian.fritsche@gmail.com>
2014-11-19 11:50:01 -0200

    fixing run.sh

--- run.sh
+++ run.sh
@@ -31,23 +31,23 @@ path="experiment/"
 
 rm -f run.txt
 
-# for objectives in $objectivesArray
-# do
-    #echo "java -cp dist/MECBA-Hyp.jar jmetal.experiments.Combined_NSGAII_"$objectives"obj" >> run.txt
-    for function in $functions
-    do
-        for problem in $problems
-        do
-        	# 2 objectives
-            echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.1027 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 3.1043 2 false $executions $path" >> run.txt
-            # 4 objectives
-            echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.0223 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 2.2274 4 false $executions $path" >> run.txt
-        done
-    done
-# done
+# # for objectives in $objectivesArray
+# # do
+#     #echo "java -cp dist/MECBA-Hyp.jar jmetal.experiments.Combined_NSGAII_"$objectives"obj" >> run.txt
+#     for function in $functions
+#     do
+#         for problem in $problems
+#         do
+#         	# 2 objectives
+#             echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.1027 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 3.1043 2 false $executions $path" >> run.txt
+#             # 4 objectives
+#             echo "java -cp dist/MECBA-Hyp.jar hyperheuristics.main.NSGAIIHyperheuristicMain $population $evaluations $crossover $mutation 1.0 0.0223 TwoPointsCrossover,MultiMaskCrossover,PMXCrossover SwapMutation,SimpleInsertionMutation $problem $function 150 2.2274 4 false $executions $path" >> run.txt
+#         done
+#     done
+# # done
 
-cat run.txt | xargs -I CMD -P 8 bash -c CMD >> output.log &
-wait
+# cat run.txt | xargs -I CMD -P 8 bash -c CMD >> output.log &
+# wait
 
 rm -f run.txt
 
